ce7d4f379ca0934f65a0ba03840063023b57b410
nicksansouci <nicksansouci@gmail.com>
2022-05-02 11:46:13 -0700

    Updated queue

---
+++ queue.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Node
+{
+	Node* next;
+	int data;
+};
+
+
+class Queue
+{
+	int length;
+	Node* front;
+	Node* temp;
+
+
+
+	Queue() : length{ 0 }, front{ nullptr }, temp{ nullptr }{}
+
+
+};
+
+
+
+
+
+
+int main()
+{
+
+}
