bdea2a6096a01094ad279d88460bd94e36898697
nicksansouci <nicksansouci@gmail.com>
2022-05-02 16:12:47 -0700

    Updated queue functions and implemented smart pointers

--- queue.cpp
+++ queue.cpp
@@ -1,23 +1,70 @@
 #include <iostream>
-
+#include <memory>
 class Node
 {
-	Node* next;
+public:
+	std::shared_ptr<Node> next;
 	int data;
 };
 
 
 class Queue
 {
+public:
 	int length;
-	Node* front;
-	Node* temp;
+	std::shared_ptr<Node> front;
+	std::shared_ptr<Node> back;
+	std::shared_ptr<Node> temp;
 
+	Queue() : length{ 0 }, front{ nullptr }, temp{ nullptr }, back{ nullptr }{}
 
-
-	Queue() : length{ 0 }, front{ nullptr }, temp{ nullptr }{}
-
-
+	~Queue() {
+		while (front)
+		{
+			front = std::move(front->next);
+		}
+	}
+	//Push an item to the back of the queue.
+	void push(int data)
+	{
+		if (length == 0)
+		{
+			front = std::shared_ptr<Node>(new Node);
+			front->data = data;
+			back = front;
+		}
+		else
+		{
+			temp = back;
+			back = std::shared_ptr<Node>(new Node);
+			back->data = data;
+			temp->next = back;
+		}
+		length++;
+	}
+	//Pop item in the front of the queue.
+	void pop()
+	{
+		if (length == 0) {
+			std::cout << "There are no items to pop from the queue.";
+			return;
+		}
+		else
+		{
+			temp = front;
+			temp = std::move(temp->next);
+			front->next = nullptr;
+			front = temp;
+		}
+	}
+	void print_queue() {
+		temp = front;
+		while (temp)
+		{
+			std::cout << temp->data << " ";
+			temp = std::move(temp->next);
+		}
+	}
 };
 
 
@@ -27,5 +74,10 @@ class Queue
 
 int main()
 {
-
+	Queue q1;
+	q1.push(6);
+	q1.push(10);
+	q1.push(8);
+	q1.pop();
+	q1.print_queue();
 }
