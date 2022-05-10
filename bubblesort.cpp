9656e83f9e84846a15162f88c5ccebb666483e42
nicksansouci <nicksansouci@gmail.com>
2022-05-10 15:51:32 -0700

    Added Selection sort

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 template <typename T>
 
-
 T BubbleSort(T& v1)
 {
 	for (int i=0; i < v1.size(); i++)
@@ -25,14 +24,36 @@ T BubbleSort(T& v1)
 }
 
 template <typename T>
-void print(T v1)
+
+T SelectionSort(T& v1)
 {
-	for (const auto& x : v1)
+	for (int i = 0; i < v1.size(); i++)
+	{
+		auto min_val = v1[i];
+		int min_index = i;
+		for (int j = i+1; j < v1.size(); j++)
+		{
+			if (v1[j] < min_val)
+			{
+				min_val = v1[j];
+				min_index = j;
+			}
+		}
+		swap(v1[i], v1[min_index]);
+	}
+	return v1;
+}
+
+template <typename T>
+void print(T value)
+{
+	for (const auto& x : value)
 	{
 		cout << x << " ";
 	}
 	cout << "\n";
 }
+
 
 
 int main()
@@ -41,7 +62,9 @@ int main()
 	BubbleSort(v2);
 	print(v2);
 
-	vector<double> v3 { 10.5, 9.1, 2.3 , 1.4, 7.8, 6.0, 8.5, 3.3, 5.5, 3.5};
-	BubbleSort(v3);
+	vector<double> v3 { 10.5, 9.1, 2.3 , 1.4, 7.8, 6.1, 8.5, 3.3, 5.5, 3.5};
+	SelectionSort(v3);
 	print(v3);
+
+	return 0;
 }
